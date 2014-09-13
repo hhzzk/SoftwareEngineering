3b95bd47ff29d618c70d361e2d6c0e975ec2c4d9
Jinrui Wang <wangjinrui2013@gmail.com>
2014-09-13 21:37:04 +0800

    Add file linklist.c and linklist.h

--- 20140908/menu.c
+++ 20140908/menu.c
@@ -18,97 +18,33 @@
  *
  */
 
-
 #include<stdio.h>
 #include<stdlib.h>
-#include<string.h>
+#include"linklist.h"
 
-#define CMD_LEN		10
-#define DESC_LEN	1024
+#define CMD_MAX_LEN		128
+#define DESC_LEN	    1024
 
-typedef struct DataNode
+int Help();
+
+/* Menu program */
+
+static tDataNode head[] = 
 {
-    char   cmd[CMD_LEN];
-    char   desc[DESC_LEN];
-    struct DataNode *next;
-} tDataNode;
-
-/* 
- * Function    : AddCmd
- * Description : Add new cmd node to cmd list 
- * Input       : 'head' is cmd list's head point address
- *               'cmd' and 'desc' are the related command and description
- * Output      : Wether succuss or fail both retun NULL
- */
-void AddCmd(tDataNode **head, char *cmd, char *desc)
-{
-    tDataNode *p = NULL;
-
-    p = (tDataNode *)malloc(sizeof(tDataNode));
-    if (p == NULL)
-    {
-        return;
-    }
-    snprintf(p->cmd, CMD_LEN, "%s", cmd);
-    snprintf(p->desc, DESC_LEN, "%s", desc);
-    p->next = *head;
-    *head = p;
-
-    return;
-}
-
-/* 
- * Function    : GetDataNode
- * Description : Get cmd list node according to 'cmd'
- * Input       : 'head' is cmd list's head point address
- *               'cmd' is the related command
- * Output      : If succuss return node address else retun NULL
- */
-tDataNode *GetDataNode(tDataNode *head, char *cmd)
-{
-    tDataNode *p = head;
-    /* Remove '/n' in the tail of cmd */
-    cmd[strlen(cmd)-1] = 0;
-
-    while(p != NULL)
-    {
-        if (strcmp(p->cmd, cmd) == 0)
-        {
-            return p;
-        }
-        p = p->next;
-    }
-
-    return NULL;
-}
+    {"help", "This is help command!", Help, &head[1]},
+    {"ls", "List information about the FILEs", NULL, NULL}
+};
 
 void main()
 {
-    tDataNode *head = NULL;
     tDataNode *p = NULL;
-    char cmd[CMD_LEN];
-
-    /* Init cmd list */
-    AddCmd(&head, "quit", "Quit this program.");
-    AddCmd(&head, "ls", "List information about the FILEs.");
-    AddCmd(&head, "cp", "Copy SOURCE to DEST, or multiple SOURCE(s) to DIRECTORY.");
-    AddCmd(&head, "ps", "Report a snapshot of the current processes.");
-    AddCmd(&head, "mv", "Move (rename) files.");
-    AddCmd(&head, "rm", "Remove files or directories.");
-
-    printf("Menu list:\n");
-    p = head;
-    while(p != NULL)
-    {
-        printf("%s - %s\n", p->cmd, p->desc);
-        p = p->next;
-    }
+    char cmd[CMD_MAX_LEN];
 
     /* Get cmd from stdin, print related description */
     while(1)
     {
         printf("Input a cmd name > ");
-        if (fgets(cmd, CMD_LEN, stdin) == NULL)
+        if (fgets(cmd, CMD_MAX_LEN, stdin) == NULL)
         {
             printf("Input Error!\n");
             continue;
@@ -120,12 +56,17 @@ void main()
             printf("This is a wrong cmd name!\n");
             continue;
         }
-        /* Handle 'quit' */
-        if (strcmp(p->cmd, "quit") == 0)
+        if (p->handler != NULL)
         {
-            free(p);
-            return;
+            p->handler();
+            continue;
         }
         printf("%s - %s\n", p->cmd, p->desc);
     }
+}
+
+int Help()
+{
+    ShowAllCmd(head);
+    return 0;
 }
